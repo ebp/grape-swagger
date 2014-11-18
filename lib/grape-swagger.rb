1b031f4963224fac4003090ca150dd4752fd7116
Doug Mayer <doug@ebackpack.com>
2014-11-18 10:48:47 -0600

    Backport entity_name override.

--- lib/grape-swagger.rb
+++ lib/grape-swagger.rb
@@ -303,10 +303,15 @@ module Grape
               version ? parsed_path.gsub('{version}', version) : parsed_path
             end
 
-            def parse_entity_name(name)
-              entity_parts = name.to_s.split('::')
-              entity_parts.reject! {|p| p == "Entity" || p == "Entities"}
-              entity_parts.join("::")
+            def parse_entity_name(model)
+              if model.respond_to?(:entity_name)
+                model.entity_name
+              else
+                name = model.to_s
+                entity_parts = name.split('::')
+                entity_parts.reject! {|p| p == "Entity" || p == "Entities"}
+                entity_parts.join("::")
+              end
             end
 
             def parse_entity_models(models)
